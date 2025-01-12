83b90c926cc7c1f5dd32dc93c0ecdb051d91710e
Jiakai Li <li.jiakai0305@gmail.com>
2025-01-13 06:16:24 +1300

    Update output

--- deployments/env_prod/outputs.tf
+++ deployments/env_prod/outputs.tf
@@ -1,6 +1,6 @@
 output "this" {
   value = {
     workspace_name = module.adb_workspace.this.workspace_name
-    resource_group = module.adb_workspace.this.resource_group
+    resource_group = module.adb_workspace.this.workspace_url
   }
 }
--- modules/workspace/outputs.tf
+++ modules/workspace/outputs.tf
@@ -1,6 +1,6 @@
 output "this" {
   value = {
     workspace_name = azurerm_databricks_workspace.this.name
-    resource_group = azurerm_databricks_workspace.this.resource_group_name
+    workspace_url = azurerm_databricks_workspace.this.workspace_url
   }
 }
